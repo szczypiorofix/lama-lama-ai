84f86e40498053dbd5a0fe7bde3ad90e4206a14d
szczypiorofix <szczypiorofix@o2.pl>
2025-05-04 11:26:54 +0200

    Define default LLM models

--- backend/src/shared/constants/LlmModels.data.ts
+++ backend/src/shared/constants/LlmModels.data.ts
@@ -1,5 +1,40 @@
-export const AVAILABLE_MODELS: string[] = [
-    'tinyllama:latest',
-    'gemma:3b',
-    'llava:7b',
+import { LlmModelEntity } from '../../orm';
+
+export const DEFAULT_LLM_MODELS: LlmModelEntity[] = [
+    {
+        id: 1,
+        name: 'tinyllama',
+        version: 'latest',
+        downloaded: false,
+        size: 0,
+        createdAt: null,
+        updatedAi: null,
+    },
+    {
+        id: 2,
+        name: 'gemma3',
+        version: '1b',
+        downloaded: false,
+        size: 0,
+        createdAt: null,
+        updatedAi: null,
+    },
+    {
+        id: 3,
+        name: 'llava',
+        version: '7b',
+        downloaded: false,
+        size: 0,
+        createdAt: null,
+        updatedAi: null,
+    },
+    {
+        id: 4,
+        name: 'gemma3',
+        version: '4b',
+        downloaded: false,
+        size: 0,
+        createdAt: null,
+        updatedAi: null,
+    },
 ];
