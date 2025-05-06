86e0eca30f1ba64c7c59558c3dd1466d69d2cd5f
szczypiorofix <szczypiorofix@o2.pl>
2025-05-06 11:18:52 +0200

    Add model purpose property to LLM model

--- frontend/src/shared/models/LlmImage.model.ts
+++ frontend/src/shared/models/LlmImage.model.ts
@@ -1,8 +1,11 @@
+import { LlmModelPurpose } from '../enums';
+
 export interface LlmImage {
     id: number;
     name: string;
     version: string;
     downloaded: boolean;
+    purpose: LlmModelPurpose;
     size: number;
     createdAt: Date | null;
     updatedAi: Date | null;
