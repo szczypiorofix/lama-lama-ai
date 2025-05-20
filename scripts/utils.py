7269f9b73837d3cf917abce8f9e24a8e8bd44606
szczypiorofix <szczypiorofix@o2.pl>
2025-05-20 06:47:36 +0200

    Add scripts for wikidata5m processing

--- scripts/utils.py
+++ scripts/utils.py
@@ -4,8 +4,9 @@ def init_chroma_collection(persist_dir: str, collection_name: str):
     client = chromadb.PersistentClient(path=persist_dir)
     return client.get_or_create_collection(name=collection_name)
 
-def insert_into_chroma(collection, texts: list[str], embeddings: list[list[float]], prefix: str = "doc"):
-    ids = [f"{prefix}-{i}" for i in range(len(texts))]
+def insert_into_chroma(collection, texts, embeddings, ids=None, prefix="doc"):
+    if ids is None:
+        ids = [f"{prefix}-{i}" for i in range(len(texts))]
     collection.add(
         documents=texts,
         embeddings=embeddings,
